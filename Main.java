3410b44c8bb1f833a1405b5b650ae454bff5a9d5
DawidWilczynski <dawid.iluzja@gmail.com>
2022-08-18 18:47:11 +0200

    Nauka zmiennych w Javie

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hej Dawid");
-        System.out.println("Super grupa");
+        String name = "Dawid";
+        int age = 30;
+        char znak = 'a';
+        int x = 10;
+        int y = 20;
+        int c = x + y;
+
+        //System.out.println("Hej "+name+ " ktory ma "+age+" lat");
+        System.out.println(c);
     }
 }
